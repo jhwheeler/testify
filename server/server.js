719f5f26910cbdccb260c63bc00db02eee95f3d0
Alacritas <jhwheeler@gmail.com>
2017-07-24 14:54:13 +0300

    Add condition to server to accommodate testing/linting
    Previously, I had to stop the server every time I ran a test or
    committed my code. This condition fixes this.

--- server/server.js
+++ server/server.js
@@ -14,19 +14,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'))
 });
 
-app.listen(PORT, () => {
-  const options = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  };
+if (!module.parent) {
+  app.listen(PORT, () => {
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
 
-  const timeNow = new Date().toLocaleTimeString('en-us', options);
+    const timeNow = new Date().toLocaleTimeString('en-us', options);
 
-  console.log(`Testify server listening on port ${PORT}: ${timeNow}`);
-});
+    console.log(`Testify server listening on port ${PORT}: ${timeNow}`);
+  });
+}
 
 module.exports = { app };
