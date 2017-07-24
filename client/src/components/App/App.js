1729e1f491331f5ef5d0e9169104b103aa4fca4c
Alacritas <jhwheeler@gmail.com>
2017-07-24 14:50:11 +0300

    Import Header into App

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import Header from '../Header/Header';
 import Button from '../Button/Button';
+
 import css from './App.scss';
 import data from '../../../../data/test-results.json';
 
 const App = () => {
   return (
-    <Button text='Analyse' />
+    <div className="app">
+      <Header text='Testify' />
+      <Button text='Analyse' />
+    </div>
   );
 }
 
